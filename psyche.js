92adb5a18f9d0071e477cbcebaf67e4640afb836
Beato Bongco <beatobongco@gmail.com>
2016-07-20 15:42:59 +0800

    Fix spacebar bug, add styles to autocomplete

--- psyche.js
+++ psyche.js
@@ -147,7 +147,7 @@ function setupPlyr() {
   })
 
   document.body.onkeyup = function(e) {
-    if(e.keyCode == 32){
+    if(e.keyCode == 32 && e.target.type !== "text"){
       p.togglePlay()
       if (e.target == document.body) {
         e.preventDefault()
